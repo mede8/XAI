53d95772ddd79fcf050e716c9fc91ec0c74b196e
Stefan Durlanescu <90749838+qThunder1@users.noreply.github.com>
2026-02-10 17:36:56 +0100

    assignment0.py

--- src/assignment0.py
+++ src/assignment0.py
@@ -1,1 +1,12 @@
-
+import json
+from anytree import Node, RenderTree, AsciiStyle
+from anytree.importer import JsonImporter
+def main():
+    importer = JsonImporter()
+    with open('coffee.json', 'r') as file:
+        data = file.read()
+    root = importer.import_(data)
+    output = RenderTree(root)
+    print(output)
+if __name__ == "__main__":
+    main()
